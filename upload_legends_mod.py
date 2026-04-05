d821210395107c9b58cb2bddbca15c3be9d5b77f
chopeks <37939738+chopeks@users.noreply.github.com>
2026-04-05 10:20:01 +0200

    added 2nd script that uploads both files

---
+++ upload_legends_mod.py
@@ -0,0 +1,69 @@
+from pathlib import Path
+from buildscript.lib import VersionExtractor, load_config
+from buildscript.python.upload import NexusUploadTask
+from buildscript.python.upload import GithubUploadTask
+
+
+def main():
+	config = load_config(Path(__file__).parent / ".build_config.py")
+	version_extractor = VersionExtractor(Path.cwd())
+
+	base_version = version_extractor.get_base_version()
+	current_version = version_extractor.extract_version()
+
+	bb_dir = Path(config.BB_DIR)
+
+	file = bb_dir / version_extractor.artifact_name_mod()
+	assets_file = bb_dir / version_extractor.artifact_name_mod()
+
+	if not file.exists():
+		print(f"{version_extractor.artifact_name_mod()} or {version_extractor.artifact_name_assets()} does not exist, calling build script")
+		import build_legends_mod
+		build_legends_mod.main()
+
+	if config.NEXUS_TOKEN is not None:
+		nexus_task = NexusUploadTask(config.NEXUS_TOKEN, 60)
+		nexus_task.run(assets_file, f"""
+This is an asset library required to play the Legends mod.
+
+It can be installed by just dropped it into data. DO NOT UNZIP.
+		""".strip())
+		nexus_task.run(file, f"""
+REQUIRES VERSION 1.5.1.0+ OF VANILLA GAME
+	
+SAVE COMPATIBLE WITH {base_version}+
+
+DO NOT UNZIP YOUR MOD FILES
+
+You need {version_extractor.artifact_name_assets()} to play!
+
+To install: drop into 'data' Battle Brothers folder
+		""".strip())
+	else:
+		print("NEXUS_TOKEN not found in config, skipping upload")
+
+	if config.GITHUB_TOKEN is not None:
+		build_name = version_extractor.extract_build_name()
+		parts = current_version.split(".")
+		previous_version = f"{'.'.join(parts[:-1])}.{int(parts[-1]) - 1}"
+
+		desc = f"""
+# {current_version} - {build_name}
+	
+`SAVE COMPATIBLE WITH {base_version}+`
+`DO NOT UNZIP YOUR MOD FILES`
+`REMOVE ALL YOUR PREVIOUS OFFICIAL LEGENDS PATCHES AND VERSIONS`
+
+Replace the `mod_legends-{previous_version}.zip` with `{version_extractor.artifact_name_mod()}`
+You need `{version_extractor.artifact_name_assets()}` to play. Download both zips.
+	
+## Fixes
+		""".strip()
+		github_task = GithubUploadTask(config.GITHUB_TOKEN, owner="Battle-Brothers-Legends", repo="Legends-public")
+		github_task.run([assets_file, file], desc, branch=f"release/{base_version}", releaseName=f"{build_name} {current_version}")
+	else:
+		print("GITHUB_TOKEN not found in config, skipping upload")
+
+
+if __name__ == "__main__":
+	main()
