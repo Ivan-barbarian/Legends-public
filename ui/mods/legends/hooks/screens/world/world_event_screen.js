a0320b42683d2bc265c630318fb762ace14bc46f
Narch <119547710+Narchanthel@users.noreply.github.com>
2026-04-14 12:15:01 +0200

    add layered item handling to world events

--- ui/mods/legends/hooks/screens/world/world_event_screen.js
+++ ui/mods/legends/hooks/screens/world/world_event_screen.js
@@ -7,4 +7,21 @@ WorldEventScreen.prototype.renderListItem = function (_container, _item)
 		lastRow.addClass("divider-top");
 	if ('divider' in _item && _item['divider'] === 'bottom')
 		lastRow.addClass("divider-bottom");
+
+	if ('imageOverlayPath' in _item && _item.imageOverlayPath !== null && _item.imageOverlayPath.length > 0)
+    {
+        var baseImage = lastRow.find('img');
+        if (baseImage.length > 0)
+        {
+            baseImage.wrap('<div class="item-image-container"/>');
+            var itemImageContainer = lastRow.find('.item-image-container');
+
+            _item.imageOverlayPath.forEach(function (path) {
+                if (path === '') return;
+                var overlayImage  = $('<img/>');
+                overlayImage .attr('src', Path.ITEMS + path);
+                itemImageContainer.append(overlayImage);
+            });
+        }
+    }
 };
