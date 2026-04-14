db89aeb31d78c258d1a754a826dc22f50eb082a8
chopeks <37939738+chopeks@users.noreply.github.com>
2026-04-14 18:27:08 +0200

    removed time measurement

--- build_brushes.py
+++ build_brushes.py
@@ -238,8 +238,4 @@ def main():
 
 
 if __name__ == "__main__":
-    import time
-    start_time = time.time()
     main()
-    end_time = time.time()
-    print(f"Execution time: {end_time - start_time} seconds")
